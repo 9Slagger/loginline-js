dd4291974afb29d8eda405ecaa216b3f9a839d04
9slagger <b5813568@g.sut.ac.th>
2020-02-19 15:21:47 +0700

    init project

---
+++ lab3.js
@@ -0,0 +1,25 @@
+const printInLine = require("./lib/PrintInLine");
+
+// table 11 * 11
+//           *         
+//           *         
+//           *         
+//           *         
+//           *         
+// * * * * * * * * * * *        
+//           *         
+//           *         
+//           *         
+//           *         
+//           *
+
+const main = () => {
+  for (let i = 0; i < 11; i++) {
+    for (let j = 0; j < 11; j++) {
+      // your code
+    }
+    printInLine.printNewLine();
+  }
+};
+
+main();
---
+++ lab5.js
@@ -0,0 +1,23 @@
+const printInLine = require("./lib/PrintInLine");
+
+// * * * * * * * * * *
+// * *             * *
+// *   *         *   *
+// *     *     *     *
+// *       * *       *
+// *       * *       *
+// *     *     *     *
+// *   *         *   *
+// * *             * *
+// * * * * * * * * * *
+
+const main = () => {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      // your code
+    }
+    printInLine.printNewLine();
+  }
+};
+
+main();
